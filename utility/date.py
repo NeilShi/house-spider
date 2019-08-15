1eb6bd35b339255426e81cb0a2576397493ee5e2
neilshi <627911861@qq.com>
2019-08-15 17:29:37 +0800

    add month and year

--- utility/date.py
+++ utility/date.py
@@ -3,6 +3,7 @@
 # 日期和时间的字符串辅助函数
 
 import time
+from datetime import date
 
 
 def get_time_string():
@@ -30,6 +31,34 @@ def get_year_month_string():
     """
     current = time.localtime()
     return time.strftime("%Y%m", current)
+
+
+def get_month(price_desc):
+    """
+    获得爬取数据的月份
+    形如 7月二手房均价 的 7
+    :return: month
+    """
+    if price_desc[0].isdecimal():
+        month = int(price_desc[0])
+    else:
+        month = date.today().month - 1
+    return month
+
+
+def get_year(month):
+    """
+    获得爬取数据的年份
+    如果是1月爬取到去年12月的数据
+    会有特殊逻辑处理
+    :return: year
+    """
+    real_month = date.today().month
+    if month > real_month:
+        year = date.today().year - 1
+    else:
+        year = date.today().year
+    return year
 
 
 if __name__ == "__main__":
