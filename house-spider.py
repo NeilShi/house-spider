79819d8f58f92729f9863877540049fb219b8320
neilshi <627911861@qq.com>
2019-08-15 14:37:00 +0800

    add city field

--- house-spider.py
+++ house-spider.py
@@ -13,8 +13,11 @@ def hello_world():
 @app.route('/community', methods=['POST'])
 def get_community():
     # error = None
+    city = json.loads(request.get_data())['city']
+    area = json.loads(request.get_data())['area']
+    district = json.loads(request.get_data())['district']
     community_spider = CommunityBaseSpider()
-    community_list = community_spider.start(request)
+    community_list = community_spider.start(city, area, district)
     response = app.response_class(
         response=json.dumps([community.__dict__ for community in community_list]),
         status=200,
@@ -24,4 +27,5 @@ def get_community():
 
 
 if __name__ == '__main__':
+    # 生产环境中务必去掉debug=True
     app.run(debug=True)
