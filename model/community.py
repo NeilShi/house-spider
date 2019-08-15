deccc4b15c2be233d81a5f058634fef0ad85832e
neilshi <627911861@qq.com>
2019-08-15 17:27:22 +0800

    add month and year

--- model/community.py
+++ model/community.py
@@ -4,13 +4,15 @@
 
 
 class Community(object):
-    def __init__(self, city, district, area, name, price, on_sale):
+    def __init__(self, city, district, area, name, price, on_sale, month, year):
         self.city = city
         self.district = district
         self.area = area
         self.price = price
         self.name = name
         self.on_sale = on_sale
+        self.month = month
+        self.year = year
 
     def text(self):
         return self.city + "," + \
@@ -18,4 +20,6 @@ class Community(object):
                 self.area + "," + \
                 self.name + "," + \
                 self.price + "," + \
-                self.on_sale
+                self.on_sale + "," + \
+                self.month + "," + \
+                self.year
