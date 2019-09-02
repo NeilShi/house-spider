8da655963e8c2fd5410e87f57ac6a4715556e4df
neilshi <627911861@qq.com>
2019-09-02 16:41:29 +0800

    add get_district() and get_area()

--- house-spider.py
+++ house-spider.py
@@ -1,6 +1,8 @@
 import json
 from flask import Flask, request
 from spider.community_spider import *
+from geo.district import get_districts
+from geo.area import get_areas
 
 app = Flask(__name__)
 
@@ -20,6 +22,37 @@ def get_community():
     community_list = community_spider.start(city, area, district)
     response = app.response_class(
         response=json.dumps([community.__dict__ for community in community_list]),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/district', methods=['POST'])
+def get_district():
+    district_list = []
+    city = json.loads(request.get_data())['city']
+    district_cn_dict = get_districts(city, True)
+    for item in district_cn_dict.items():
+        district_list.append({item[0]: item[1]})
+    response = app.response_class(
+        response=json.dumps(district_list),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/area', methods=['POST'])
+def get_area():
+    area_list = []
+    city = json.loads(request.get_data())['city']
+    district = json.loads(request.get_data())['district']
+    area_cn_dict = get_areas(city, district, True)
+    for item in area_cn_dict.items():
+        area_list.append({item[0]: item[1]})
+    response = app.response_class(
+        response=json.dumps(area_list),
         status=200,
         mimetype='application/json'
     )
